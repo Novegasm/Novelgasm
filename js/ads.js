2bf5c85e9089caf578ed8f2c8489789cfde18e1a
Novegasm <izikadekunle@gmail.com>
2024-11-14 20:04:36 +0100

    Update ads.js

--- js/ads.js
+++ js/ads.js
@@ -1,7 +1,7 @@
-// Create a script element for the Google AdSense script
-const adsenseScript = document.createElement('script');
-adsenseScript.async = true;
-adsenseScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9840840799346727"
-     crossorigin="anonymous";
+// Load the Google AdSense script
+const script = document.createElement("script");
+script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9840840799346727";
+script.crossOrigin = "anonymous";
+document.head.appendChild(script);
 
 
