6b7cc30cc9cdeb25e940f012556dbe2dfbe2babb
Novegasm <izikadekunle@gmail.com>
2024-11-19 00:47:47 +0100

    Delete js directory

--- js/ads.js
+++
@@ -1,7 +0,0 @@
-// Load the Google AdSense script
-const script = document.createElement("script");
-script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9840840799346727";
-script.crossOrigin = "anonymous";
-document.head.appendChild(script);
-
-
